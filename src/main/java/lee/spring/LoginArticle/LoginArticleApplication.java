6e4088d6d30f36e0cae20f7639e5ab244a8fabd8
LeeTaeSuk <twohugerock@gmail.com>
2023-12-03 12:59:12 +0900

    프로젝트 첫번째 커밋

---
+++ src/main/java/lee/spring/LoginArticle/LoginArticleApplication.java
@@ -0,0 +1,13 @@
+package lee.spring.LoginArticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginArticleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lee/spring/LoginArticle/LoginArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.spring.LoginArticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
